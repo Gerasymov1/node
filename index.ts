2edcaee0b3c20625693b39820f8b7beae90f25c3
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-08-17 01:27:50 +0300

    add basic endpoints

--- index.ts
+++ index.ts
@@ -1,16 +1,146 @@
 import express from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
+import { mockedUsers, PORT } from "./src/constants";
+import { User } from "./src/types";
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (_, res) => {
-  res.status(200).json({ message: "Hello World" });
+  res.status(201).send({ message: "Hello World" });
+});
+
+app.get("/api/users", (request, response) => {
+  const {
+    query: { filter, value },
+  } = request;
+
+  if (filter && value) {
+    const users = mockedUsers.filter((user: User) => {
+      const userValue = user[filter as keyof User];
+
+      if (typeof userValue === "string" && typeof value === "string") {
+        return userValue.toLowerCase().includes(value.toLowerCase());
+      }
+
+      return false;
+    });
+
+    return response.send(users);
+  }
+
+  return response.send(mockedUsers);
+});
+
+app.get("/api/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) {
+    response.status(400).send({ message: "Invalid ID" });
+  }
+
+  const user = mockedUsers.find((user) => user.id === parsedId);
+
+  if (!user) {
+    response.sendStatus(404);
+  }
+
+  response.send(user);
+});
+
+app.post("/api/users", (request, response) => {
+  const { username, displayName } = request.body;
+
+  if (!username || !displayName) {
+    response.status(400).send({ message: "Invalid data" });
+  }
+
+  const id = mockedUsers[mockedUsers.length - 1].id + 1;
+
+  const newUser = {
+    id,
+    username,
+    displayName,
+  };
+
+  mockedUsers.push(newUser);
+
+  return response.status(201).send(newUser);
+});
+
+app.put("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) {
+    return response.sendStatus(400);
+  }
+
+  const userIndex = mockedUsers.findIndex((user) => user.id === parsedId);
+
+  if (userIndex === -1) {
+    return response.sendStatus(404);
+  }
+
+  mockedUsers[userIndex] = {
+    id: parsedId,
+    ...body,
+  };
+
+  return response.sendStatus(204);
+});
+
+app.patch("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) {
+    return response.sendStatus(400);
+  }
+
+  const userIndex = mockedUsers.findIndex((user) => user.id === parsedId);
+
+  if (userIndex === -1) {
+    return response.sendStatus(404);
+  }
+
+  mockedUsers[userIndex] = {
+    ...mockedUsers[userIndex],
+    ...body,
+  };
+
+  return response.sendStatus(204);
+});
+
+app.delete("/api/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) {
+    return response.sendStatus(400);
+  }
+
+  const userIndex = mockedUsers.findIndex((user) => user.id === parsedId);
+
+  if (userIndex === -1) {
+    return response.sendStatus(404);
+  }
+
+  mockedUsers.splice(userIndex, 1);
+
+  return response.sendStatus(204);
 });
 
 app.listen(PORT, () => {
---
+++ src/constants/index.ts
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+import { User } from "../types";
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000;
+
+export const mockedUsers: User[] = [
+  {
+    id: 1,
+    username: "john doe",
+    displayName: "John Doe",
+  },
+  {
+    id: 2,
+    username: "jane doe",
+    displayName: "Jane Doe",
+  },
+  {
+    id: 3,
+    username: "john smith",
+    displayName: "John Smith",
+  },
+  {
+    id: 4,
+    username: "jane smith",
+    displayName: "Jane Smith",
+  },
+  {
+    id: 5,
+    username: "john wick",
+    displayName: "John Wick",
+  },
+  {
+    id: 6,
+    username: "jane wick",
+    displayName: "Jane Wick",
+  },
+  {
+    id: 7,
+    username: "john snow",
+    displayName: "John Snow",
+  },
+  {
+    id: 8,
+    username: "jane snow",
+    displayName: "Jane Snow",
+  },
+  {
+    id: 9,
+    username: "john cena",
+    displayName: "John Cena",
+  },
+  {
+    id: 10,
+    username: "jane cena",
+    displayName: "Jane Cena",
+  },
+];
