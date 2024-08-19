d1402f8000e5337caa2d92fcac5e8a76b9a06133
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-08-19 18:19:33 +0300

    add options request check

--- src/middlewares/headers.ts
+++ src/middlewares/headers.ts
@@ -8,13 +8,18 @@ export const handleSetHeaders = (
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader(
     "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, DELETE"
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   response.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization"
   );
   response.setHeader("Content-Type", "application/json");
+
+  if ("OPTIONS" === request.method) {
+    response.sendStatus(200);
+    return;
+  }
 
   next();
 };
