9ccc8912940c2e0255b350d14777043b3cc98f4e
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-08-22 00:45:58 +0300

    add new types

--- src/types/users.ts
+++ src/types/users.ts
@@ -3,3 +3,10 @@ export type User = {
   username: string;
   displayName: string;
 };
+
+export type UserDB = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  refreshToken: string;
+};
---
+++ src/types/usersChats.ts
@@ -0,0 +1,5 @@
+export type UserChat = {
+  id: number;
+  userId: number;
+  chatId: number;
+};
