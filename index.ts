686fad6307d7fe3d49d7948fa5dccf829aacb0f5
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-08-29 00:44:57 +0300

    add script to run create tables

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import { PORT } from "./src/constants";
 import router from "./src/routes";
 import { handleSetHeaders } from "./src/middlewares";
 
-import { initializeTables } from "./src/models";
 import { UserDB } from "./src/types";
 
 declare global {
@@ -30,12 +29,6 @@ app.get("/", (_, res) => {
   res.status(201).send({ message: "Hello World" });
 });
 
-const start = () => {
-  initializeTables().then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-  });
-};
-
-start();
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
--- src/models/index.ts
+++ src/models/index.ts
@@ -4,10 +4,20 @@ import { createUsersChatsTable } from "./usersChats";
 import { createMessagesTable } from "./messages";
 import { createRefreshTokensTable } from "./refreshTokens";
 
-export const initializeTables = async () => {
+const initializeTables = async () => {
   await createUsersTable();
   await createChatsTable();
   await createUsersChatsTable();
   await createMessagesTable();
   await createRefreshTokensTable();
 };
+
+initializeTables()
+  .then(() => {
+    console.log("Tables initialized");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
