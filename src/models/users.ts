d069ce0b281c0731bae65ad4ab6141f3d75e67db
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-09-02 01:27:39 +0300

    change users table request

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,35 +1,27 @@
-import { db } from "../settings/db";
+import connection from "../settings/db";
 
 export const createUsersTable = async () => {
   const checkIfTableExists = `SHOW TABLES LIKE 'Users';`;
 
-  db.query(checkIfTableExists, (err, result) => {
-    if (err) {
-      console.error("Error checking if users table exists: ", err);
-      return;
-    }
+  const result = await connection.query(checkIfTableExists);
 
-    if ((result as []).length > 0) {
-      return;
-    }
+  if (!result) {
+    console.error("Error checking if Users table exists");
+    return;
+  }
 
-    console.log("Creating users table");
+  const query = `CREATE TABLE IF NOT EXISTS Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  );`;
 
-    const query = `CREATE TABLE IF NOT EXISTS Users (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        firstName VARCHAR(255) NOT NULL,
-        lastName VARCHAR(255) NOT NULL,
-        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-      );`;
-
-    db.query(query, (err) => {
-      if (err) {
-        console.error("Error creating users table: ", err);
-        return;
-      }
-
-      console.log("Users table created");
-    });
-  });
+  try {
+    await connection.query(query);
+    console.log("Users table created");
+  } catch (error) {
+    console.error(error);
+  }
 };
