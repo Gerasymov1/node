2a641dca07a5905d5ab68d26d0c2f5a582cd9071
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-09-14 01:27:56 +0300

    update migrations logic

---
+++ src/migrations/1726265847619_add_createdAt_column.ts
@@ -0,0 +1,16 @@
+import connection from "../settings/db";
+
+export const up = async () => {
+  const query = `ALTER TABLE Migrations ADD COLUMN createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP;`;
+
+  try {
+    await connection.query(query);
+    console.log("Migration table altered");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const down = async () => {
+  // Write your migration here
+};
---
+++ src/migrations/1726266305846_remove_createdAt_column.ts
@@ -0,0 +1,16 @@
+import connection from "../settings/db";
+
+export const up = async () => {
+  const query = `ALTER TABLE Migrations DROP COLUMN createdAt;`;
+
+  try {
+    await connection.query(query);
+    console.log("Migration table altered");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const down = async () => {
+  // Write your migration here
+};
