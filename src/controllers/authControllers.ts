4e8a6ce21475163ee30bb902b3fb37452558853e
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-09-25 21:02:54 +0300

    wrap code in try catch

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -13,92 +13,127 @@ type ExtendedQueryResult = {
 export const login = async (req: Request, res: Response) => {
   const { password, email } = req.body;
 
-  if (!password || !email) {
+  try {
+    if (!password || !email) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .info("Invalid request, fill in all fields");
+      return res.badRequest("Invalid request, fill in all fields");
+    }
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .info("User not found");
+      return res.notFound("User not found");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .info("Invalid password or email");
+      return res.unauthorized("Invalid password or email");
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user.id);
+
+    if (!accessToken || !refreshToken) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .error("Error setting refresh token");
+      return res.internalServerError("Error setting refresh token");
+    }
+
+    res
+      .cookie("refreshToken", refreshToken)
+      .cookie("accessToken", accessToken)
+      .success({ user }, "Logged in");
+  } catch (error) {
     logger
       .child({
         childData: {
           email,
         },
       })
-      .info("Invalid request, fill in all fields");
-    return res.badRequest("Invalid request, fill in all fields");
+      .error("Error logging in");
+    return res.internalServerError("Error logging in");
   }
-
-  const user = await findUserByEmail(email);
-
-  if (!user) {
-    logger
-      .child({
-        childData: {
-          email,
-        },
-      })
-      .info("User not found");
-    return res.notFound("User not found");
-  }
-
-  const match = await bcrypt.compare(password, user.password);
-
-  if (!match) {
-    logger
-      .child({
-        childData: {
-          email,
-        },
-      })
-      .info("Invalid password or email");
-    return res.unauthorized("Invalid password or email");
-  }
-
-  const { accessToken, refreshToken } = await generateTokens(user.id);
-
-  if (!accessToken || !refreshToken) {
-    logger
-      .child({
-        childData: {
-          email,
-        },
-      })
-      .error("Error setting refresh token");
-    return res.internalServerError("Error setting refresh token");
-  }
-
-  res
-    .cookie("refreshToken", refreshToken)
-    .cookie("accessToken", accessToken)
-    .success({ user }, "Logged in");
 };
 
 export const register = async (req: Request, res: Response) => {
   const { firstName, lastName, password, email } = req.body;
 
-  const searchedUser = await findUserByEmail(email);
+  try {
+    if (!firstName || !lastName || !password || !email) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .info("Invalid request, fill in all fields");
+      return res.badRequest("Invalid request, fill in all fields");
+    }
 
-  if (!!searchedUser) {
-    logger
-      .child({
-        childData: {
-          email,
-        },
-      })
-      .info("User already exists");
-    return res.conflict("User already exists");
-  }
+    const searchedUser = await findUserByEmail(email);
 
-  const saltRounds = 10;
+    if (!!searchedUser) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .info("User already exists");
+      return res.conflict("User already exists");
+    }
 
-  const hash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
 
-  const user = {
-    firstName,
-    lastName,
-    password: hash,
-    email,
-  };
+    const hash = await bcrypt.hash(password, saltRounds);
 
-  const result = await createUser(user as User);
+    const user = {
+      firstName,
+      lastName,
+      password: hash,
+      email,
+    };
 
-  if ((result as ExtendedQueryResult).insertId === 0) {
+    const result = await createUser(user as User);
+
+    if ((result as ExtendedQueryResult).insertId === 0) {
+      logger
+        .child({
+          childData: {
+            email,
+          },
+        })
+        .error("Error creating user");
+      return res.internalServerError("Error creating user");
+    }
+
+    res.created({ user }, "User created");
+  } catch (error) {
     logger
       .child({
         childData: {
@@ -108,6 +143,4 @@ export const register = async (req: Request, res: Response) => {
       .error("Error creating user");
     return res.internalServerError("Error creating user");
   }
-
-  res.created({ user }, "User created");
 };
