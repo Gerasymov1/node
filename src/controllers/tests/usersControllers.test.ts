1e928f862a64b0be32f94e84613ea4c77f90ac59
Gerasymov1 <a.k.gerasymov@gmail.com>
2024-10-14 15:10:26 +0300

    add mocha config

---
+++ src/controllers/tests/usersControllers.test.ts
@@ -0,0 +1,17 @@
+describe("Users Controller", () => {
+  it("should return a list of users", () => {
+    // test implementation
+  });
+  it("should return a user by id", () => {
+    // test implementation
+  });
+  it("should create a new user", () => {
+    // test implementation
+  });
+  it("should update a user", () => {
+    // test implementation
+  });
+  it("should delete a user", () => {
+    // test implementation
+  });
+});
